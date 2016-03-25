ffa080cf923dac13a350043961897f091a78a62e
aaronistheman <aarons.7007@gmail.com>
2016-03-25 15:11:58 -0700

    Added scripts to make opening certain files easier
    These scripts are from my Useful Shell Scripts repository.

---
+++ add-file.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# @param $1 name of the file to create.
+# @example add-file.sh hello-world.txt
+
+if [ ! -e open-my-files.bat ]
+then # create the file with proper format
+  echo "exit" > open-my-files.bat
+fi
+
+# Create and open the file
+touch $1
+start Notepad++ $1
+
+# Edit the batch file that opens files (if applicable)
+sed -i "s~exit~start Notepad++ $1\nexit~" open-my-files.bat
