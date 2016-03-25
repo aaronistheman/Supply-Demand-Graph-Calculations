1d4c1f725bb4ba5633c46e0ba7ead61d2f6a2a14
aaronistheman <aarons.7007@gmail.com>
2016-03-25 16:01:09 -0700

    Removed use of data-ng-init
    I've heard it's better to use the controller (for initializing variables).
    Furthermore, having the data-ng-init directives scattered around
    index.html seems like a problem waiting to happen.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+"use strict";
+
 // The module must have a name, or else an error occurs
 var angularJS = angular.module("app", []).controller('ctrl', function($scope) {
+  // The initializations (I'd rather they're here than scattered around
+  // with a bunch of data-ng-init directives)
   $scope.isClosed = true;
+  $scope.isPublic = false;
+  $scope.taxed = "supply";
+  $scope.subsidized = "supply";
+  $scope.priceMechanism = "none";
 });
 
 $(document).ready(function() {
