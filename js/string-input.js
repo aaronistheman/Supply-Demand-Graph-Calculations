2462c7fb5c6c4faa93eb3d4b0e516a00e5fcf00f
aaronistheman <aarons.7007@gmail.com>
2016-06-09 19:42:57 -0700

    Cleaned up StringInput; changed get() to getChar()

--- js/string-input.js
+++ js/string-input.js
@@ -4,13 +4,25 @@
  * Custom type to handle the parsing of a string of inputs;
  * is kind of like an "input stream", I suppose
  */
-function StringInput(theString) {
+function StringInput(theString) { // constructor
   if (!(this instanceof StringInput)) // if caller forgot "new"
     return new StringInput(theString);
 
   this._stream = theString;
+  this._state = StringInput.states.OK;
 }
 
+/**
+ * StringInput "static" variables
+ */
+StringInput.states = {
+  OK : 1,
+  END : 2,
+};
+
+/**
+ * StringInput methods
+ */
 StringInput.prototype = {
   constructor : StringInput,
 
@@ -18,15 +30,21 @@ StringInput.prototype = {
     this._stream = this._stream.slice(1);
   },
 
+  getState : function() {
+    return this._state;
+  },
+
   /**
    * @returns next char in the "input stream", then advances the stream;
    * returns "" if end reached
    */
-  get : function() {
+  getChar : function() {
     var ch = this._stream[0];
 
-    if (ch === undefined) // if reached end
+    if (ch === undefined) { // if reached end
+      this._state = StringInput.states.END;
       return "";
+    }
     else {
       this._advanceStream();
       return ch;
@@ -40,6 +58,14 @@ StringInput.prototype = {
   ignore : function(charToIgnore) {
     while (this._stream[0] === charToIgnore) // until no more to ignore
       this._advanceStream();
+  },
+
+  getNumber : function() {
+
+  },
+
+  ignoreUntil : function() {
+
   },
 
 }; // StringInput.prototype
--- js/test.js
+++ js/test.js
@@ -2,24 +2,24 @@
 
 var currentTestedFile = "string-input.js";
 
-QUnit.module(currentTestedFile + ", StringInput.get()");
+QUnit.module(currentTestedFile + ", StringInput.getChar()");
 
   QUnit.test("correct return", function(assert) {
     var sin = new StringInput("abcd");
-    assert.deepEqual(sin.get(), 'a');
+    assert.deepEqual(sin.getChar(), 'a');
   });
 
   QUnit.test("advances stream", function(assert) {
     var sin = new StringInput("abcd");
-    sin.get();
-    assert.deepEqual(sin.get(), 'b');
+    sin.getChar();
+    assert.deepEqual(sin.getChar(), 'b');
   });
 
   QUnit.test("returns empty string if nothing left", function(assert) {
     var sin = new StringInput("ab");
-    sin.get();
-    sin.get();
-    assert.deepEqual(sin.get(), '');
+    sin.getChar();
+    sin.getChar();
+    assert.deepEqual(sin.getChar(), '');
   });
 
 QUnit.module(currentTestedFile + ", StringInput.ignore()");
@@ -27,11 +27,11 @@ QUnit.module(currentTestedFile + ", StringInput.ignore()");
   QUnit.test("ignores appropriate chars", function(assert) {
     var sin = new StringInput("aaaabaacd");
     sin.ignore('a');
-    assert.deepEqual(sin.get(), 'b');
+    assert.deepEqual(sin.getChar(), 'b');
   });
 
   QUnit.test("can ignore nothing", function(assert) {
     var sin = new StringInput("abcd");
     sin.ignore('b');
-    assert.deepEqual(sin.get(), 'a');
+    assert.deepEqual(sin.getChar(), 'a');
   });
