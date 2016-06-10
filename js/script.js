8c5f1a5312118661ab81d952e9cd2db37db40a0f
aaronistheman <aarons.7007@gmail.com>
2016-06-09 18:46:26 -0700

    Set up QUnit for unit testing
    I had to separate out the AngularJS code.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 "use strict";
 
-// The module must have a name, or else an error occurs
-var angularJS = angular.module("app", []).controller('ctrl', function($scope) {
-  // The initializations (I'd rather they're here than scattered around
-  // with a bunch of data-ng-init directives)
-  $scope.isClosed = true;
-  $scope.isPublic = false;
-  $scope.taxed = "supply";
-  $scope.subsidized = "supply";
-  $scope.priceMechanism = "none";
-  $scope.tradeFactor = "none";
-}); // Angular JS controller
-
 function graphSupply(inputString) {
   var canvas = document.getElementById("graph");
   var ctx = canvas.getContext("2d");
@@ -31,8 +19,10 @@ function graphSupply(inputString) {
 } // graphSupply()
 
 $(document).ready(function() {
-  // Hardcoded data; eventually read from file
-  var supplyPoints = "40 0.25 ; 90 0.75 ; 110 1.35";
+  if (!isUnitTesting()) {
+    // Hardcoded data; eventually read from file
+    var supplyPoints = "40 0.25 ; 90 0.75 ; 110 1.35";
 
-  graphSupply(supplyPoints);
+    graphSupply(supplyPoints);
+  }
 }); // document ready function
---
+++ js/test.js
@@ -0,0 +1,10 @@
+"use strict";
+
+QUnit.module("riemann.js, RiemannGenerator.getLeftRiemannSum()");
+
+  var numRectsArray = [10, 100, 500, 1000, 5000, 10000, 500000, 1000000];
+
+  QUnit.test("linear: left riemann is lower than real integral",
+    function(assert) {
+    ;
+  });
