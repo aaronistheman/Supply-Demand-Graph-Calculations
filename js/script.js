c50adb647fd0871c83236d40007327569c6b0d3f
aaronistheman <aarons.7007@gmail.com>
2016-06-09 22:24:00 -0700

    Drew (unlabelled) axes (to see how they'd look)

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 "use strict";
 
+var offsetX = 10;
+var offsetY = 10;
+var maxX = 120;
+var maxY = 1.50;
+
+function graphAxes() {
+  var canvas = document.getElementById("graph");
+  var ctx = canvas.getContext("2d");
+  ctx.moveTo(offsetX, canvas.height - offsetY);
+  ctx.lineTo(canvas.width, canvas.height - offsetY);
+  ctx.moveTo(offsetX, canvas.height - offsetY);
+  ctx.lineTo(offsetX, 0);
+  ctx.stroke();
+}
+
 function graphSupply(inputString) {
   var canvas = document.getElementById("graph");
   var ctx = canvas.getContext("2d");
   // ctx.fillStyle = "black";
   // ctx.fillRect(30, 30, 150, 75);
-
-  var maxX = 120;
-  var maxY = 1.50;
 
   var q = -1;
   var p = -1;
@@ -36,11 +48,11 @@ function graphSupply(inputString) {
   }
 
   // Draw the lines
-  ctx.moveTo(canvas.width * qArray[0] / maxX,
-    canvas.height - canvas.height * pArray[0] / maxY);
+  ctx.moveTo(canvas.width * qArray[0] / maxX + offsetX,
+    canvas.height - canvas.height * pArray[0] / maxY + offsetY);
   for (var i in qArray) {
-    ctx.lineTo(canvas.width * qArray[i] / maxX,
-      canvas.height - canvas.height * pArray[i] / maxY);
+    ctx.lineTo(canvas.width * qArray[i] / maxX + offsetX,
+      canvas.height - canvas.height * pArray[i] / maxY + offsetY);
   }
   ctx.stroke();
 } // graphSupply()
@@ -51,6 +63,7 @@ $(document).ready(function() {
     var supplyPoints = "40 0.25 ; 50 0.30 ; 90 0.75 ; 110 1.35";
     var demandPoints = "40 1.25 ; 80 0.75 ; 110 0.25";
 
+    graphAxes();
     graphSupply(supplyPoints);
     graphSupply(demandPoints);
   }
