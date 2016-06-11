c3d7341887d377583d980b23733ce92799207166
aaronistheman <aarons.7007@gmail.com>
2016-06-11 16:39:32 -0700

    Added another test for Graph.getEquilibriumPoint()

--- js/test.js
+++ js/test.js
@@ -164,6 +164,9 @@ QUnit.module(currentTestedFile + ", calculateLowestQuantity()");
 QUnit.module(currentTestedFile + ", getEquilibriumPoint()");
 
   QUnit.test("correct point found", function(assert) {
+    // In this test, the intersection point is (pretty much
+    // a given point
+    
     var supplyPoints = "40 0.25 ; 50 0.30 ; 90 0.75 ; 110 1.35";
     var demandPoints = "40 1.25 ; 60 0.90 ; 90 0.75 ; 110 0.30";
     var graph = new Graph(supplyPoints, demandPoints);
@@ -176,4 +179,21 @@ QUnit.module(currentTestedFile + ", getEquilibriumPoint()");
     
     assert.deepEqual(eqPoint.x, 90);
     assert.deepEqual(eqPoint.y, 0.75);
+  });
+
+  QUnit.test("correct point found again!", function(assert) {
+    // In this test, the intersection point isn't a given point
+    
+    var supplyPoints = "40 0.25 ; 50 0.30 ; 90 0.70 ; 110 1.35";
+    var demandPoints = "40 1.25 ; 60 0.75 ; 80 0.25 ; 110 0.20";
+    var graph = new Graph(supplyPoints, demandPoints);
+    
+    // Get the point and do appropriate rounding to make unit
+    // testing more useful
+    var eqPoint = graph.getEquilibriumPoint();
+    eqPoint.x = Math.round(eqPoint.x);
+    eqPoint.y = Math.round(eqPoint.y * 100) / 100;
+    
+    assert.deepEqual(eqPoint.x, 70);
+    assert.deepEqual(eqPoint.y, 0.50);
   });
