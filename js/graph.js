f830cbc3a1cd0c0c657b90cd6be2545cadee69d3
aaronistheman <aarons.7007@gmail.com>
2016-06-11 10:07:30 -0700

    Method to clear the canvas

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/**
+ * Graph custom type that's specifically for this project but
+ * perhaps has general use.
+ *
+ * Note that the beginPath method of the canvas context is sometimes
+ * called seemingly randomly, to clear the canvas' strokes
+ * (so clearRect() would do something).
+ */
 function Graph(supplyDataString, demandDataString) {
   if (!(this instanceof Graph))
     return new Graph();
@@ -65,12 +73,25 @@ Graph._readFunctionData = function(func, dataString) {
 }; // readFunctionData()
 
 /**
+ * This method is needed because of the context offset
+ */
+Graph._clearCanvas = function(canvas, ctx) {
+  ctx.beginPath();
+  ctx.clearRect(-Graph.OFFSET_X * Graph.MAX_X,
+    -Graph.OFFSET_Y * Graph.MAX_Y,
+    canvas.width * Graph.MAX_X,
+    canvas.height * Graph.MAX_Y);
+};
+
+/**
  * Methods for Graph
  */
 Graph.prototype = {
   constructor : Graph,
 
   drawAxes : function() {
+    this._ctx.beginPath();
+
     // x-axis
     this._ctx.moveTo(0, 0);
     this._ctx.lineTo(this._canvas.width * Graph.MAX_X, 0);
@@ -88,6 +109,7 @@ Graph.prototype = {
    */
   _drawGraph : function(points) {
     // Move to the first point
+    this._ctx.beginPath();
     this._ctx.moveTo(this._canvas.width * points[0].x,
       this._canvas.height * points[0].y);
     for (var i in points) {
@@ -99,10 +121,12 @@ Graph.prototype = {
   },
 
   redrawSupply : function() {
+    Graph._clearCanvas(this._canvas, this._ctx);
     this._drawGraph(this._supply.getPoints());
   },
 
   redrawDemand : function() {
+    Graph._clearCanvas(this._canvas, this._ctx);
     this._drawGraph(this._demand.getPoints());
   },
 };
