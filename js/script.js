675ea0557313cd0fc0e24ec35c0eebc32d9a9f44
aaronistheman <aarons.7007@gmail.com>
2016-06-13 10:12:53 -0700

    Consumer surplus is displayed on main webpage

--- js/script.js
+++ js/script.js
@@ -12,12 +12,16 @@ $(document).ready(function() {
     graph.redrawDemand();
     
     var eq = graph.getEquilibriumPoint();
+    document.getElementById("eq-q").innerHTML = Math.round(eq.x);
     document.getElementById("eq-p").innerHTML =
       (Math.round(eq.y * 100) / 100).toFixed(2);
-    document.getElementById("eq-q").innerHTML = Math.round(eq.x);
     
     var tr = eq.x * eq.y;
     document.getElementById("total-rev").innerHTML =
       (Math.round(tr * 100) / 100).toFixed(2);
+      
+    var cs = graph.getConsumerSurplus();
+    document.getElementById("con-s").innerHTML =
+      (Math.round(cs * 100) / 100).toFixed(2);
   }
 }); // document ready function
