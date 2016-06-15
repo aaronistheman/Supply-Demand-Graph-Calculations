7c6b32192375c11cf88f8f9a63bb8dfde1fe4257
aaronistheman <aarons.7007@gmail.com>
2016-06-15 15:41:24 -0700

    Severely updated todo list

--- open-my-files.bat
+++ open-my-files.bat
@@ -5,7 +5,7 @@ start Notepad++ js/script.js
 start Notepad++ js/string-input.js
 start Notepad++ test.html
 start Notepad++ js/test.js
-start Notepad++ js/controller.js
+REM start Notepad++ js/controller.js
 start Notepad++ js/piecewise-function.js
 start Notepad++ js/graph.js
 exit
