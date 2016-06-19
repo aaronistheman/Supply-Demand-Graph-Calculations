da49e00b2ba7ddb5b644b751528ce0f83d7d30b5
aaronistheman <aarons.7007@gmail.com>
2016-06-18 23:34:31 -0700

    Added price and quantity custom types
    I haven't applied them though.

---
+++ js/utility/price.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * Wrapper type so I don't have to keep manually rounding the price
+ */
+function Price(val) {
+  this.mVal = val;
+}
+
+Price.prototype = {
+  constructor : Price,
+  
+  /**
+   * Gets the price, but rounded as a price would be (i.e. to nearest
+   * hundreth)
+   */
+  get : function() {
+    return Math.round(val * 100) / 100;
+  },
+};
