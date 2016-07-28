e58c74f45af3307f8e854248ab949fe0e58eea28
aaronistheman <aarons.7007@gmail.com>
2016-07-27 18:55:08 -0700

    Removed getY()
    Only the to-be-removed Graph custom type uses it

--- js/model/piecewise-function.js
+++ js/model/piecewise-function.js
@@ -24,42 +24,6 @@ PiecewiseFunction.prototype = {
   getPoints : function() {
     return this._points;
   },
-
-  /*
-  getY : function(x) {
-    if (x < this._points[0].x
-      || x > this._points[this._points.length - 1])
-      alertAndThrowException("x out of range");
-
-    // find the first point below x and first point above it
-    var before = null;
-    var after = null;
-    var afterIndex;
-    for (afterIndex = 1; afterIndex < this._points.length
-      && this._points[afterIndex].x < x; ++afterIndex)
-      ;
-
-    if (afterIndex < this._points.length) { // if didn't reach end
-      before = this._points[afterIndex - 1];
-      after = this._points[afterIndex];
-
-      var slope = (after.y - before.y) / (after.x - before.x);
-      return before.y + (x - before.x) * slope;
-    }
-    else {
-      /**
-       * Note: because of the earlier range check, this "should"
-       * never happen, but JavaScript has imperfect comparison
-       * of floating-point numbers (e.g. 1.450000000000000000016 > 1.45
-       * returns false); however, this coincidentally allows me to
-       * here make a fix regarding the possibility of Riemann sums
-       * going a little bit outside of range
-       */
-/*
-      return this._points[this._points.length - 1].y;
-    }
-  }, // getY()
-  */
   
   /**
    * @param q just the quantity value (NOT instance of Quantity)
