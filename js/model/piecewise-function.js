19752e8ec67fec4d83104645cd7d54caca56c5f6
aaronistheman <aarons.7007@gmail.com>
2016-07-27 18:24:51 -0700

    Rewrote PiecewiseFunction.getY() as getP()

--- js/model/piecewise-function.js
+++ js/model/piecewise-function.js
@@ -62,11 +62,44 @@ PiecewiseFunction.prototype = {
   */
   
   /**
-   * @param q 
+   * @param q just the quantity value (NOT instance of Quantity)
    */
   getP : function(q) {
-    
-  },
+    if (q < this._points[0].q()
+      || q > this._points[this._points.length - 1].q())
+      alertAndThrowException("x out of range");
+
+    // find the first point below x and first point above it
+    var before = null;
+    var after = null;
+    var afterIndex; // needed after for loop
+    // afterIndex will have index of first point above x
+    for (afterIndex = 1; afterIndex < this._points.length
+      && this._points[afterIndex].q() < q; ++afterIndex)
+      ;
+
+    if (afterIndex < this._points.length) { // if didn't reach end
+      before = this._points[afterIndex - 1];
+      after = this._points[afterIndex];
+
+      // Get the slope between the two points, then use that slope
+      // to approximate/interpolate what the price-to-return is
+      var slope = (after.p() - before.p()) / (after.q() - before.q());
+      return before.p() + (q - before.q()) * slope;
+    }
+    else {
+      /**
+       * Note: because of the earlier range check, this "should"
+       * never happen, but JavaScript has imperfect comparison
+       * of floating-point numbers (e.g. 1.450000000000000000016 > 1.45
+       * returns false); however, this coincidentally allows me to
+       * here make a fix regarding the possibility of Riemann sums
+       * going a little bit outside of range.
+       */
+
+      return this._points[this._points.length - 1].p();
+    }
+  }, // getP()
 
   /**
    * @param point instance of Point
