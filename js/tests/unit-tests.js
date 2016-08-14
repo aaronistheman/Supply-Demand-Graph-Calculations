5dd560c2c6fe62581697d37d81632f8ebdebe3de
aaronistheman <aarons.7007@gmail.com>
2016-08-14 12:03:32 -0700

    Changed test.js to unit-tests.js

--- js/tests/unit-tests.js
+++ js/tests/unit-tests.js
@@ -186,6 +186,26 @@ QUnit.module(currentTestedFile +
     assert.deepEqual(data.getConsumerSurplus().get(),
       23.50); // hand-calculated
   });
+  
+  /*
+  QUnit.test("correct value yet again!", function(assert) {
+    var data = new EconomyModel(
+      "30 0.60 ; 60 0.90 ; 90 1.20",
+      "30 0.90 ; 60 0.60 ; 90 0.30");
+    
+    assert.deepEqual(data.getConsumerSurplus().get(),
+      1.13); // hand-calculated
+  });
+  */
+  
+  QUnit.test("correct value yet again!", function(assert) {
+    var data = new EconomyModel(
+      "30 0.30 ; 60 0.60 ; 90 0.90",
+      "30 0.90 ; 60 0.60 ; 90 0.30");
+    
+    assert.deepEqual(data.getConsumerSurplus().get(),
+      4.50); // hand-calculated
+  });
 
 QUnit.module(currentTestedFile +
   ", EconomyModel.prototype.getProducerSurplus()");
@@ -264,7 +284,63 @@ QUnit.module(currentTestedFile +
     var result = Quantity.get(data.calculateWorldQs());
     assert.deepEqual(result, 60); // correct value isn't a point
   });
+  
+QUnit.module(currentTestedFile + ", EconomyModel.prototype.setTax()");
 
+  QUnit.test("correct equilibrium point", function(assert) {
+    var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
+    var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
+    var data = new EconomyModel(supplyPoints, demandPoints);
+    
+    data.setTax(Graph.Supply, 0.3);
+    assert.deepEqual(data.eq, 45);
+    assert.deepEqual(data.ep, 0.75);
+  });
+  
+  /*
+  QUnit.test("demand tax: correct consumer surplus adjustment",
+    function(assert) {
+    var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
+    var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
+    var data = new EconomyModel(supplyPoints, demandPoints);
+    
+    data.setTax(Graph.Demand, 0.3);
+    assert.deepEqual(data.getConsumerSurplus().get(),
+      400); // hand-calculated
+  });
+  
+  QUnit.test("supply tax: correct consumer surplus adjustment",
+    function(assert) {
+    var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
+    var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
+    var data = new EconomyModel(supplyPoints, demandPoints);
+    
+    data.setTax(Graph.Supply, 0.3);
+    assert.deepEqual(data.getConsumerSurplus().get(),
+      1.13); // hand-calculated
+  });
+  
+  QUnit.test("correct supply offset from tax", function(assert) {
+    var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
+    var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
+    var data = new EconomyModel(supplyPoints, demandPoints);
+    
+    // data.setTax(Graph.Supply, 0.3);
+    assert.deepEqual(Price.get(data.getSupply().getP(45)), 0.75);
+  });
+  
+  QUnit.test("correct supply offset from subsidy", function(assert) {
+    
+  });
+  
+  QUnit.test("correct demand offset from subsidy", function(assert) {
+    
+  });
+  
+  QUnit.test("correct demand offset from tax", function(assert) {
+    
+  });
+  */
 currentTestedFile = "model/string-input.js";
 QUnit.module(currentTestedFile + ", StringInput.prototype.getChar()");
 
