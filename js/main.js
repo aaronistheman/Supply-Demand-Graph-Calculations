51f42d45ee06f3a3edce29fcd38bde5962a90956
aaronistheman <aarons.7007@gmail.com>
2016-08-16 23:48:59 -0700

    Severely updated todo list with more linear plan-of-action

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 $(document).ready(function() {
   if (!isUnitTesting()) {
     // Hardcoded data; eventually read from file
-    var supplyPoints = "40 0.25 ; 50 0.30 ; 90 0.75 ; 110 1.35";
-    var demandPoints = "30 1.25 ; 90 0.75 ; 110 0.30 ; 120 0.15";
-    // var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
-    // var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
+    // var supplyPoints = "40 0.25 ; 50 0.30 ; 90 0.75 ; 110 1.35";
+    // var demandPoints = "30 1.25 ; 90 0.75 ; 110 0.30 ; 120 0.15";
+    var supplyPoints = "30 0.30 ; 60 0.60 ; 90 0.90";
+    var demandPoints = "30 0.90 ; 60 0.60 ; 90 0.30";
     var data = new EconomyModel(supplyPoints, demandPoints);
     
     var textView = new TextView("Glue");
