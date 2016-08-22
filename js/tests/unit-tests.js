92b5d9de21a14b8b3aeb05f2ab8ba5c896b202fb
aaronistheman <aarons.7007@gmail.com>
2016-08-21 23:07:01 -0700

    Moved quantity.js tests

--- js/tests/unit-tests.js
+++ js/tests/unit-tests.js
@@ -2,27 +2,7 @@
 
 var currentTestedFile = undefined;
 
-currentTestedFile = "utility/quantity.js";
-QUnit.module(currentTestedFile + ", Quantity.prototype.get()");
 
-  QUnit.test("correct return", function(assert) {
-    var q = new Quantity(3.63);
-    assert.deepEqual(q.get(), 4);
-  });
-
-QUnit.module(currentTestedFile + ", Quantity.prototype.getUnrounded()");
-
-  QUnit.test("correct return", function(assert) {
-    var q = new Quantity(3.63);
-    assert.deepEqual(q.getUnrounded(), 3.63);
-  });
-  
-QUnit.module(currentTestedFile + ", Quantity.get()");
-
-  QUnit.test("correct return", function(assert) {
-    var q = Quantity.get(3.634529);
-    assert.deepEqual(q, 4);
-  });
 
 currentTestedFile = "utility/price.js";
 QUnit.module(currentTestedFile + ", Price.prototype.get()");
