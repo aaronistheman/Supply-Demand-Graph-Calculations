42314b4bc58eebe6b442cf623fb16fa35dfdb3cb
aaronistheman <aarons.7007@gmail.com>
2016-08-21 23:16:13 -0700

    Reorganized most of unit tests

---
+++ js/tests/unit-tests/piecewise-function/test-piecewise-function-all.js
@@ -0,0 +1,60 @@
+var currentTestedFile = "model/piecewise-function.js";
+
+QUnit.module(currentTestedFile +
+  ", PiecewiseFunction.prototype.getP()");
+
+  QUnit.test("returns correct price", function(assert) {
+    var pf = new PiecewiseFunction();
+    pf.insert(new Point(5, 8));
+    pf.insert(new Point(9, 16));
+    
+    var price = pf.getP(8);
+    assert.deepEqual(price, 14);
+  });
+  
+  QUnit.test("returns correct price #2", function(assert) {
+    var pf = new PiecewiseFunction();
+    pf.insert(new Point(1, 7));
+    pf.insert(new Point(5, 6));
+    pf.insert(new Point(7, 4));
+    pf.insert(new Point(9, 3));
+    
+    var price = pf.getP(8);
+    assert.deepEqual(price, 3.50);
+  });
+  
+  QUnit.test("returns correct offset price #1", function(assert) {
+    var pf = new PiecewiseFunction();
+    pf.insert(new Point(5, 8));
+    pf.insert(new Point(9, 16));
+    
+    var price = pf.getP(8, 2.30);
+    assert.deepEqual(price, 16.30);
+  });
+  
+  QUnit.test("returns correct offset price #2", function(assert) {
+    var pf = new PiecewiseFunction();
+    pf.insert(new Point(1, 7));
+    pf.insert(new Point(5, 6));
+    pf.insert(new Point(7, 4));
+    pf.insert(new Point(9, 3));
+    
+    var price = pf.getP(8, -1.10);
+    assert.deepEqual(price, 2.40);
+  });
+
+QUnit.module(currentTestedFile +
+  ", PiecewiseFunction.prototype.insert()");
+
+  QUnit.test("error-checking works", function(assert) {
+    var caughtError = false;
+    var pf = new PiecewiseFunction();
+    pf.insert(new Point(5, 8));
+    try {
+      pf.insert(new Point(4, 9));
+    }
+    catch (err) {
+      caughtError = true;
+    }
+    assert.ok(caughtError, "Successfully caught error");
+  });
---
+++ js/tests/unit-tests/price/test-price-all.js
@@ -0,0 +1,46 @@
+var currentTestedFile = "utility/price.js";
+
+QUnit.module(currentTestedFile + ", Price.prototype.get()");
+
+  QUnit.test("correct return", function(assert) {
+    var p = new Price(5.789222);
+    assert.deepEqual(p.get(), 5.79);
+  });
+
+QUnit.module(currentTestedFile + ", Price.prototype.getUnrounded()");
+
+  QUnit.test("correct return", function(assert) {
+    var p = new Price(5.789222);
+    assert.deepEqual(p.getUnrounded(), 5.789222);
+  });
+
+QUnit.module(currentTestedFile + ", Price.prototype.forDisplay()");
+
+  QUnit.test("correct return", function(assert) {
+    var p = new Price(5.7);
+    assert.deepEqual(p.forDisplay(), "$5.70");
+  });
+
+  QUnit.test("correct return", function(assert) {
+    var p = new Price(5.789222);
+    assert.deepEqual(p.forDisplay(), "$5.79");
+  });
+
+QUnit.module(currentTestedFile + ", Price.get()");
+
+  QUnit.test("correct return", function(assert) {
+    var p = Price.get(5.789222);
+    assert.deepEqual(p, 5.79);
+  });
+
+QUnit.module(currentTestedFile + ", Price.forDisplay()");
+
+  QUnit.test("correct return", function(assert) {
+    var p = Price.forDisplay(2.8351);
+    assert.deepEqual(p, "$2.84");
+  });
+
+  QUnit.test("correct return #2", function(assert) {
+    var p = Price.forDisplay(5.7);
+    assert.deepEqual(p, "$5.70");
+  });
