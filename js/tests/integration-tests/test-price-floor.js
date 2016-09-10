7d381ae03f0db0be15919d4d62d7c0a94a18dcec
aaronistheman <aarons.7007@gmail.com>
2016-09-10 14:40:25 -0700

    Integration tests for changing price floor's amount

--- js/tests/integration-tests/test-price-floor.js
+++ js/tests/integration-tests/test-price-floor.js
@@ -1,11 +1,11 @@
 "use strict";
 
-QUnit.module("price ceiling's amount updated");
+QUnit.module("price floor's amount updated");
 
   QUnit.test("unchanged domestic equilibrium", function(assert) {
     var data = getLinearGraph1();
     
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.40);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.70);
     assert.deepEqual(data.eq, 60);
     assert.deepEqual(data.ep, 0.60);
   });
@@ -13,40 +13,39 @@ QUnit.module("price ceiling's amount updated");
   QUnit.test("correct quantity demanded", function(assert) {
     var data = getLinearGraph1();
     
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.45);
-    assert.deepEqual(data.qd, 75);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.75);
+    assert.deepEqual(data.qd, 45);
   });
   
   QUnit.test("correct quantity supplied", function(assert) {
     var data = getLinearGraph1();
     
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.45);
-    assert.deepEqual(data.qs, 45);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.75);
+    assert.deepEqual(data.qs, 75);
   });
   
   QUnit.test("correct state", function(assert) {
     var data = getLinearGraph1();
     
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.45);
-    assert.deepEqual(data.getState(), States.Shortage);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.75);
+    assert.deepEqual(data.getState(), States.Surplus);
   });
   
   QUnit.test("correct domestic producers' revenue", function(assert) {
     var data = getLinearGraph1();
-    var priceCeiling = 0.45;
-    var qs = 45;
-    data.setPriceMechanismAmount(Mechanism.Ceiling, priceCeiling);
+    var priceFloor = 0.75;
+    var qd = 45;
+    data.setPriceMechanismAmount(Mechanism.Floor, priceFloor);
     
-    assert.deepEqual(data.getTotalRevenue().get(),
-      Price.get(priceCeiling * qs));
+    assert.deepEqual(data.getTotalRevenue().get,
+      Price.get(priceFloor * qd));
   });
   
   QUnit.test("correct consumer surplus", function(assert) {
     var data = getLinearGraph1();
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.45);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.75);
     
-    var handCalculatedCS = (45 - 30) * (0.75 - 0.45)
-      + ((45 - 30) * (0.90 - 0.75) / 2);
+    var handCalculatedCS = (45 - 30) * (0.90 - 0.75) / 2;
     
     assert.deepEqual(data.getConsumerSurplus().get(),
       Price.get(handCalculatedCS));
@@ -54,9 +53,10 @@ QUnit.module("price ceiling's amount updated");
   
   QUnit.test("correct producer surplus", function(assert) {
     var data = getLinearGraph1();
-    data.setPriceMechanismAmount(Mechanism.Ceiling, 0.45);
+    data.setPriceMechanismAmount(Mechanism.Floor, 0.75);
     
-    var handCalculatedPS = (45 - 30) * (0.45 - 0.30) / 2;
+    var handCalculatedPS = (45 - 30) * (0.75 - 0.45)
+      + (45 - 30) * (0.45 - 0.30) / 2;
     
     assert.deepEqual(data.getProducerSurplus.get(),
       Price.get(handCalculatedPS));
