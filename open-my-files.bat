a304dac8863ad4da8b5420a2fd0f382de3b8578a
aaronistheman <aarons.7007@gmail.com>
2016-09-11 11:57:55 -0700

    Reorganized integration tests

--- open-my-files.bat
+++ open-my-files.bat
@@ -7,13 +7,12 @@ start Notepad++ test.html
 REM start Notepad++ js/controller.js
 REM start Notepad++ js/model/piecewise-function.js
 start Notepad++ js/model/economy-model.js
-REM start Notepad++ js/model/constants.js
+start Notepad++ js/model/constants.js
 start Notepad++ js/model/point.js
-start Notepad++ js/views/text-view.js
-start Notepad++ js/views/graph-view.js
+REM start Notepad++ js/views/text-view.js
+REM start Notepad++ js/views/graph-view.js
 start Notepad++ js/controllers/settings-controller.js
 REM start Notepad++ js/controllers/emphasis-controller.js
-start Notepad++ js/tests/sample-graphs.js
+REM start Notepad++ js/tests/sample-graphs.js
 start Notepad++ open-my-files.bat
-start Notepad++ js/tests/integration-tests/*
 exit
