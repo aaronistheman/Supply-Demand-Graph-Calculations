57414dfa12c352b089fa37fb374ecbe027b0f14f
aaronistheman <aarons.7007@gmail.com>
2016-09-11 21:29:14 -0700

    Changed location of constants.js, since SettingsController uses it

--- open-my-files.bat
+++ open-my-files.bat
@@ -7,7 +7,7 @@ start Notepad++ test.html
 REM start Notepad++ js/controller.js
 REM start Notepad++ js/model/piecewise-function.js
 start Notepad++ js/model/economy-model.js
-start Notepad++ js/model/constants.js
+start Notepad++ js/utility/constants.js
 start Notepad++ js/model/point.js
 REM start Notepad++ js/views/text-view.js
 REM start Notepad++ js/views/graph-view.js
